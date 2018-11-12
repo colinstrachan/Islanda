f398c84be78b283275f84d6136ee83dbbee7ee06
Francisco Di Persia <fdipersia@gmail.com>
2018-11-12 15:36:27 -0300

    Correct booking routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :islands
-  resources :users, only: [:show] do
-    resources :bookings, except: [:edit, :update]
+  resources :islands do
+    resources :bookings, only: [:new, :create]
   end
+  resources :users, only: [:show] do
+    resources :bookings, only: [:index]
+  end
+  resources :bookings, only: [:show, :destroy]
 end
