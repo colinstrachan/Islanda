d80d30c485a21d68dbb8228677b872a4bfb362d0
Colin Strachan <col@strachanmusic.com>
2018-11-13 13:13:54 -0300

    fixed permission issue with creating islands

--- app/policies/island_policy.rb
+++ app/policies/island_policy.rb
@@ -3,25 +3,21 @@ class IslandPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+  end
 
-    def index
-      return true
-    end
+  def show?
+    return true
+  end
 
-    def show
-      return true
-    end
+  def create?
+    return true
+  end
 
-    def create
-      return true
-    end
+  def update?
+    record.user == user
+  end
 
-    def update
-      record.user == user
-    end
-
-    def destroy
-      record.user == user
-    end
+  def destroy?
+    record.user == user
   end
 end
