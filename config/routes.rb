798b7acadfeca13af7e2f6536045716ecf1d416a
Francisco Di Persia <fdipersia@gmail.com>
2018-11-13 10:50:20 -0300

    Add search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "islands/search", to: "islands#search"
   resources :islands
   resources :users, only: [:show] do
     resources :bookings, except: [:edit, :update]
