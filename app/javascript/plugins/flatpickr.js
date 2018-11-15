c210a4f5adf0376b75b9fa16ce6ace1cfda7275b
Colin Strachan <col@strachanmusic.com>
2018-11-15 16:13:13 -0300

    completed booking system

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,9 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
-flatpickr(".datepicker", {mode: 'range', minDate: "today"})
+const booked_dates_node = document.getElementById("booked_dates")
+const booked_dates = booked_dates_node.dataset.bookeddates
+const parsed_dates = JSON.parse(booked_dates)
+console.log(parsed_dates)
+
+flatpickr(".datepicker", {mode: 'range', minDate: "today", dateFormat: "Y-m-d", disable: parsed_dates})
